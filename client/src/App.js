8f3cc4975d311289436ae7bc5b40f1c440c4073d
hoslack <hoslackochieng@gmail.com>
2018-02-07 17:12:54 +0200

    Added buttons with category relates #2

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import "tachyons/css/tachyons.css";
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'tachyons/css/tachyons.css';
+import Landing from './components/Landing';
+import Navbar from './components/Navbar';
+import CategoryForm from './components/CategoryForm';
 
-import Landing from "./components/Landing";
-import Navbar from "./components/Navbar";
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Navbar />
-        <Route path="/" component={Landing} />
-      </div>
-    </BrowserRouter>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/category" component={CategoryForm} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <header className="bg-orange fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+    <header className="bg-orange w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
       <nav className="f6 fw6 ttu tracked tc">
-        <a
-          className="f6 link dim br3 ph3 pv2 mb2 dib orange bg-white"
-          href="#0"
-          title="Home"
-        >
+        <a className="f6 link dim br3 ph3 pv2 mb2 dib orange bg-white" href="/" title="Home">
           View Reports
         </a>
       </nav>

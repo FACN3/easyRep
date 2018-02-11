5cddca586a99f268c604be6a98215d2ed1cd28e6
Marlen Awwad <marlenawsh3@gmail.com>
2018-02-11 12:51:57 +0200

    Added the icons for categories and fixed requested issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import 'tachyons/css/tachyons.css';
-import Landing from './components/Landing';
-import Navbar from './components/Navbar';
-import CategoryForm from './containers/CategoryForm';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Landing from "./components/Landing";
+import Navbar from "./components/Navbar";
+import CategoryForm from "./containers/CategoryForm";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/categories" component={CategoryForm} />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/categories" component={CategoryForm} />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- client/src/reducers/category_list.js
+++ client/src/reducers/category_list.js
@@ -1,9 +1,39 @@
+import airPollution from "../icons/1.png";
+import waterPollution from "../icons/2.png";
+import wasteDisposal from "../icons/3.png";
+import badInfra from "../icons/4.png";
+
 export default function() {
   return [
-    {name: 'Waste Burning', icon: 'https://cdn0.iconfinder.com/data/icons/large-weather-icons/512/Heat.png'},
-    {name: 'Dust', icon: ''},
-    {name: 'Water Pollution', icon: ''},
-    {name: 'Noise Pollution', icon: ''},
-    {name: 'Bad Infrastructures', icon: ''}
+    {
+      name: "Waste Disposal",
+      icon: wasteDisposal,
+      alt: "Waste Burning icon"
+    },
+    {
+      name: "Air Pollution",
+      icon: airPollution,
+      alt: "Air Pollution icon"
+    },
+    {
+      name: "Water Pollution",
+      icon: waterPollution,
+      alt: "Water Pollution icon"
+    },
+    {
+      name: "Bad Infrastructures",
+      icon: badInfra,
+      alt: "Bad Infrastructures icon"
+    },
+    {
+      name: "Waste Burning",
+      icon: "",
+      alt: "Waste Burning icon"
+    },
+    {
+      name: "Noise",
+      icon: "",
+      alt: "Noise icon"
+    }
   ];
 }

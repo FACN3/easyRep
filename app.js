035139484eb1b27c06f86d335f30bd0a91a393cf
Marlen Awwad <marlenawsh3@gmail.com>
2018-02-13 19:18:06 +0200

    Routing in production mode in order to deploy the app to heroku
    Reference #25

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('env2')('./config.env');
+const keys = require('./config/keys');
 const reportRoutes = require('./routes/reportRoutes');
+
+process.on('unhandledRejection', up => {
+  throw up;
+}); //in Node 8.2.1 if theres an error, the errors will not crash the program. Instead a warning is printed..so this code
+// is here in order for that warning to go away and showing the error like usual
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = 5000 || process.env.PORT;
 const host = 'localhost' || process.env.HOST;
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(keys.MONGO_URL);
 
 mongoose.connection.on('error', function(err) {
   console.log('Mongoose default connection error: ' + err);
@@ -30,4 +46,6 @@ app.get('/', (req, res) => {
 
 app.use('/api', reportRoutes);
 
-
+app.listen(`${port}`, () => {
+  console.log(`Express server listening on port https://localhost:${port}`);
+});
--- config/prod.js
+++ config/prod.js
@@ -1,1 +1,3 @@
-module.exports = {};
+module.exports = {
+  mongoURL: process.env.MONGO_URL
+};

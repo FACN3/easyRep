72355ba6789550bd17b3c8052872ea29fc1477a5
MynahMarie <mynah.marie@gmail.com>
2018-02-20 11:05:53 +0200

    Cleaning up

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-CURRENCY_API_KEY: '231eb90015c1884b34ef94c0c0c3b4bc',
-IMAGE_UPLOAD_PRESET: 'i50l2x2r',
-IMAGE_API_SECRET: 'S253wP6tc6hhr2G69DzzWdpi7Gw',
-IMAGE_API_KEY: '986696942147982',
-CLOUD_NAME: 'df2ethf4o',
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}

e96e40ba323816aa2882b7e3b3b56e97ecebc921
hoslack <hoslackochieng@gmail.com>
2018-02-21 12:47:57 +0200

    changed router name, and solved some bugs relates #19

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,34 +1,26 @@
-const authRouter = require('express').Router();
+const router = require('express').Router();
 const passport = require('passport');
 const requireLogin = require('../middlewares/requireLogin');
 
-authRouter.get('/facebook', passport.authenticate('facebook',
-    { scope: ['email'] }
-  )
-);
+router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 
-authRouter.get('/facebook/callback', passport.authenticate('facebook'),
-  (req, res) => {
-    res.redirect('/viewreports');
+router.get('/facebook/callback', passport.authenticate('facebook'), (req, res) => {
+  res.redirect('/viewreports');
 });
 
-authRouter.get('/google', passport.authenticate('google',
-    { scope: ['email'] }
-  )
-);
+router.get('/google', passport.authenticate('google', { scope: ['email'] }));
 
-authRouter.get('/google/callback', passport.authenticate('google'),
-  (req, res) => {
-    res.redirect('/viewreports');
+router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+  res.redirect('/viewreports');
 });
 
-authRouter.get('/current_user', requireLogin, (req, res) => {
-    res.redirect('/viewreports');
+router.get('/current_user', requireLogin, (req, res) => {
+  res.redirect('/viewreports');
 });
 
-authRouter.get('/logout', (req, res) => {
+router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
-})
+});
 
-module.exports = authRouter;
+module.exports = router;
